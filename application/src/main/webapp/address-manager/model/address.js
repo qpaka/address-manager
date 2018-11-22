861043223e16684484ff9f3d37a860f33d52ffb9
qpaka <45140869+qpaka@users.noreply.github.com>
2018-11-22 18:07:40 +0100

    Add files via upload

---
+++ application/src/main/webapp/address-manager/model/address.js
@@ -0,0 +1,23 @@
+"use strict";
+
+sap.ui.define([
+], function () {
+    return {
+        create: function (oAddressData, sBusinessPartnerId) {
+            if (oAddressData) {
+                oAddressData.isNew = false;
+                return oAddressData;
+            }
+            return {
+                BusinessPartner: sBusinessPartnerId,
+                CityName: "",
+                Country: "",
+                HouseNumber: "",
+                PostalCode: "",
+                StreetName: "",
+                AddressTimeZone: "",
+                isNew: true
+            };
+        }
+    };
+});
